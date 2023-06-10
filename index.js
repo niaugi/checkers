fa73d2a067b90a299dc3743b383c80d219b39764
niaugi <niaugi@yahoo.co.uk>
2023-06-10 01:01:06 +0100

    refact

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
-let x
-
 let board = document.getElementById('board');
 let boardArray = [];
-let whiteCheckersInit = [
+let whiteInitPos = [
     [7, 0], [7, 2], [7, 4], [7, 6],
     [6, 1], [6, 3], [6, 5], [6, 7],
-    [5, 0], [5, 2], [5, 4], [5, 6]
-]
-
-let blackCheckersInit = [
+    [5, 0], [5, 2], [5, 4], [5, 6]]
+let blackInitPos = [
     [0, 1], [0, 3], [0, 5], [0, 7],
     [1, 0], [1, 2], [1, 4], [1, 6],
-    [2, 1], [2, 3], [2, 5], [2, 7]
-]
-
-let emptySquare = [
+    [2, 1], [2, 3], [2, 5], [2, 7]]
+let emptyInitPos = [
     [3, 0], [3, 2], [3, 4], [3, 6],
-    [4, 1], [4, 3], [4, 5], [4, 7]
-]
+    [4, 1], [4, 3], [4, 5], [4, 7]]
 
-let rowToRemove = 0;
-let colToRemove = 0;
 let direction = Number;
 let playerColor = ''
-
+let currentRow = 0;
+let currentCol = 0;
+//* ------------- START -----------------
 createBoard();
-placeCheckers();
+placeCheckers();    //! placeCheckers has eventListener - clickSquare 
 
 function placeCheckers() {
 
-    placeFigure(whiteCheckersInit, 'O');
-    placeFigure(blackCheckersInit, 'X');
-    placeFigure(emptySquare, '');
+    placeFigure(whiteInitPos, 'O');
+    placeFigure(blackInitPos, 'X');
+    placeFigure(emptyInitPos, '');
 
-    function placeFigure(checkersColor, checkerFigure) {
-        for (let idS = 0; idS < checkersColor.length; idS++) {
-            let row = checkersColor[idS][0]
-            let col = checkersColor[idS][1]
+    //* Function called 3 times to place Whites, Blacks and Empties
+    function placeFigure(checkerColor, checkerFigure) {
+        for (let idS = 0; idS < checkerColor.length; idS++) {
+            let row = checkerColor[idS][0]
+            let col = checkerColor[idS][1]
             let elementId = `pos${row}-${col}`
             document.getElementById(elementId).innerText = checkerFigure;
-            document.getElementById(elementId).style.fontWeight = 'bold';
             document.getElementById(elementId).addEventListener('click', clickSquare)
         }
     }
@@ -55,8 +48,6 @@ function createBoard() {
             }
         }
     }
-
-    console.log(boardArray);
 
     for (let rows = 0; rows < 8; rows++) {
         let row = document.createElement('div');
@@ -76,44 +67,42 @@ function createBoard() {
 
 function clickSquare() {
 
-    //coming from addEventListener - this.id == 
+
+    //! coming from addEventListener: this.id
     console.log(this.id);
     let thisSquare = document.getElementById(this.id)
 
-
-
     if (thisSquare.innerText == 'O') {
         playerColor = 'O'
-        rowToRemove = this.id[3]
-        colToRemove = this.id[5]
+        currentRow = this.id[3]
+        currentCol = this.id[5]
     }
     else if (thisSquare.innerText == 'X') {
         playerColor = 'X'
-        rowToRemove = this.id[3]
-        colToRemove = this.id[5]
+        currentRow = this.id[3]
+        currentCol = this.id[5]
     }
     else if (thisSquare.innerText == '') {
         let newRow = this.id[3]
         let newCol = this.id[5]
-        console.log(`esam else if, playerColor = ${playerColor}`)
+        console.log(`else if, playerColor = ${playerColor}`)
         if (playerColor) {
-            //! adding logic if move is legal:
             if (testMove()) {
                 //* removes old checker
-                document.getElementById(`pos${rowToRemove}-${colToRemove}`).innerText = ''
+                document.getElementById(`pos${currentRow}-${currentCol}`).innerText = ''
                 //* add new checker
                 thisSquare.innerText = playerColor;
             }
 
+            //* adding logic if move is legal:
             function testMove() {
 
                 let stepForward = 0
                 let stepSides = 0
-                // console.log('testMove function data: ' + newRow + newCol)
-                if (playerColor == 'O') { stepForward = rowToRemove - newRow }
-                if (playerColor == 'X') { stepForward = newRow - rowToRemove }
+                if (playerColor == 'O') { stepForward = currentRow - newRow }
+                if (playerColor == 'X') { stepForward = newRow - currentRow }
                 if (stepForward == 1) {
-                    stepSides = Math.abs(newCol - colToRemove)
+                    stepSides = Math.abs(newCol - currentCol)
                     console.log({ stepSides })
                     if (stepSides == 1) {
                         return true
